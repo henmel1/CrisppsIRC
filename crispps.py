ca7666acb1f236d68ad54ef6a8aa4500c7b2092a
henmel1 <henmel@vt.edu>
2023-08-02 00:40:31 -0400

    GUI updated, now displays chat within window

--- crispps.py
+++ crispps.py
@@ -5,11 +5,16 @@ import threading
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 window = tk.Tk()
+chat_frame = tk.Frame(window)
+text_box = tk.Text(chat_frame, height=40, width=80, pady=10, padx=10)
+chat_frame.pack()
+text_box.pack()
+text_box.insert(tk.END, 'Hello!')
 
 
 def check_for_ping():
     text = irc.recv(2040)
-    print(text.decode())
+    text_box.insert(tk.END, "\n"+text.decode())
 
     if text.find('PING'.encode()) != -1:
         irc.send(('PONG ' + text.split(":".encode())[1].decode()).encode())
@@ -36,7 +41,9 @@ irc.send(("JOIN " + channel + "\n").encode())
 textEnter = tk.Entry(window, width=24)
 textEnter.pack()
 textEnter.place(anchor='center', relx=0.5, rely=0.9)
-button = tk.Button(window, bd=5, text="Send", command=lambda: [irc.send(("PRIVMSG " + channel + " :" + textEnter.get() + "\r\n").encode()), textEnter.delete(0, 'end')])
+button = tk.Button(window, bd=5, text="Send", command=lambda: [irc.send(("PRIVMSG " + channel + " :" + textEnter.get() + "\r\n").encode()),
+                                                               text_box.insert(tk.END, "\n"+nick+": "+textEnter.get()),
+                                                               textEnter.delete(0, 'end')])
 button.pack()
 button.place(anchor='center', relx=0.70, rely=0.9)
 
