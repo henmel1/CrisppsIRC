bf6ad2cee6f183474c773bd3f2ed8e52e4e7d082
henmel1 <henmel@vt.edu>
2023-08-02 17:24:44 -0400

    Made UI scalable, added enter function to send messages, comments

--- crispps.py
+++ crispps.py
@@ -7,12 +7,16 @@ server = "irc.libera.chat"
 channel = "#bot-test"
 nick = "nonerroneousname414132"
 
+# create irc socket and chat feed frame
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 window = tk.Tk()
+window.grid_columnconfigure(0, weight=1)
+window.grid_rowconfigure(0, weight=1)
 chat_frame = tk.Frame(window)
-text_box = tk.Text(chat_frame, height=40, width=80, pady=10, padx=10)
-chat_frame.pack()
-text_box.pack()
+text_box = tk.Text(chat_frame, pady=10, padx=10, cursor="arrow")
+text_box.bind("<Key>", lambda e: "break")
+chat_frame.grid(row=0, column=0, columnspan=4, sticky='nswe')
+text_box.pack(expand=True, fill='both')
 
 
 def format_text(text):
@@ -20,17 +24,16 @@ def format_text(text):
         n1 = text.index('!')
         n2 = text.index('#')
     except:
-        n1 = -1
         n2 = -1
-    if(n1 > -1 and n2 > -1):
+    if n2 > -1:
         name = text[1:n1]
         message = text[n2+len(channel)+2:]
-        if 'JOIN' in text:
+        if 'JOIN' in text[n1:n2]:
             return name+' joined.'
-        elif 'QUIT' in text:
-            return name+' disconnected.'
         else:
             return name + ': ' + message
+    elif '!' in text and 'QUIT' in text:
+        return text[1:text.index('!')] + ' disconnected.'
     else:
         return text
 
@@ -38,6 +41,7 @@ def format_text(text):
 def check_for_ping():
     text = irc.recv(2040)
     text_box.insert(tk.END, "\n"+format_text(text.decode()))
+    text_box.see("end")  # autoscroll to latest message
 
     if text.find('PING'.encode()) != -1:
         irc.send(('PONG ' + text.split(":".encode())[1].decode()).encode())
@@ -48,27 +52,34 @@ def update_chat():
         check_for_ping()
 
 
-print("connecting to: "+server)
-irc.connect((server, 6667))
-irc.send(("NICK " + nick + "\n").encode())
-check_for_ping()
-irc.send(("USER " + nick + " 0 * :" + nick + "\n").encode())
-check_for_ping()
-time.sleep(8)
-irc.send(("JOIN " + channel + "\n").encode())
+def connect_to_channel():
+    text_box.insert(tk.END, "connecting to: "+server)
+    irc.connect((server, 6667))
+    irc.send(("NICK " + nick + "\n").encode())
+    check_for_ping()
+    irc.send(("USER " + nick + " 0 * :" + nick + "\n").encode())
+    check_for_ping()
+    time.sleep(8)
+    irc.send(("JOIN " + channel + "\n").encode())
 
-textEnter = tk.Entry(window, width=24)
-textEnter.pack()
-textEnter.place(anchor='center', relx=0.5, rely=0.9)
+
+# connect to the given server and channel
+connect_to_channel()
+
+# create and format chat entry and send button
+textEnter = tk.Entry(window)
 button = tk.Button(window, bd=5, text="Send", command=lambda: [irc.send(("PRIVMSG " + channel + " :" + textEnter.get() + "\r\n").encode()),
                                                                text_box.insert(tk.END, "\n"+nick+": "+textEnter.get()),
+                                                               text_box.see("end"),
                                                                textEnter.delete(0, 'end')])
-button.pack()
-button.place(anchor='center', relx=0.70, rely=0.9)
+textEnter.bind("<Return>", (lambda event: button.invoke()))  # allows user to hit enter to send message
+textEnter.grid(row=1, column=0, sticky='ew')
+button.grid(row=1, column=1, sticky='w')
 
+# start main window and start updating chat
 window.update_idletasks()
 thread = threading.Thread(target=update_chat)
 thread.start()
 window.after(200, button.invoke)  # prevents needing to press button twice (bug?)
-window.geometry("800x800")
+window.title("CrisppsIRC")
 window.mainloop()
