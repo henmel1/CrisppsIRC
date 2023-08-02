b12f2a0438dec08e931b136388efcb81c4e6bc44
henmel1 <henmel@vt.edu>
2023-08-02 01:02:52 -0400

    Added cleaner chat, joining and disconnecting messages

--- crispps.py
+++ crispps.py
@@ -3,18 +3,41 @@ import time
 import tkinter as tk
 import threading
 
+server = "irc.libera.chat"
+channel = "#bot-test"
+nick = "nonerroneousname414132"
+
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 window = tk.Tk()
 chat_frame = tk.Frame(window)
 text_box = tk.Text(chat_frame, height=40, width=80, pady=10, padx=10)
 chat_frame.pack()
 text_box.pack()
-text_box.insert(tk.END, 'Hello!')
+
+
+def format_text(text):
+    try:
+        n1 = text.index('!')
+        n2 = text.index('#')
+    except:
+        n1 = -1
+        n2 = -1
+    if(n1 > -1 and n2 > -1):
+        name = text[1:n1]
+        message = text[n2+len(channel)+2:]
+        if 'JOIN' in text:
+            return name+' joined.'
+        elif 'QUIT' in text:
+            return name+' disconnected.'
+        else:
+            return name + ': ' + message
+    else:
+        return text
 
 
 def check_for_ping():
     text = irc.recv(2040)
-    text_box.insert(tk.END, "\n"+text.decode())
+    text_box.insert(tk.END, "\n"+format_text(text.decode()))
 
     if text.find('PING'.encode()) != -1:
         irc.send(('PONG ' + text.split(":".encode())[1].decode()).encode())
@@ -24,10 +47,6 @@ def update_chat():
     while 1:
         check_for_ping()
 
-
-server = "irc.libera.chat"
-channel = "#bot-test"
-nick = "nonerroneousname414132"
 
 print("connecting to: "+server)
 irc.connect((server, 6667))
